3755d86fda1f111bc3df89d2ed11ab66727aeff5
Rohit Pandey <153757342+Rohit3Pandey@users.noreply.github.com>
2024-04-07 23:19:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def index():
     return render_template('index.html', cpu_metric=cpu_metric, mem_metric=mem_metric)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
